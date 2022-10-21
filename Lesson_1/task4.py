68798e89c3139abb37ceaa0ffb5dbb8c8ebbe5f7
Stanislav <slipenko40@gmail.com>
2022-10-21 20:35:40 +0300

    does not give an x value with a negative discriminant

--- Lesson_1/task4.py
+++ Lesson_1/task4.py
@@ -6,4 +6,9 @@ koren = b*b-4*a*c
 deskrem = koren**0.5
 x1 = (-b+deskrem)/2*a
 x2 = (-b-deskrem)/2*a
-print(str(a)+'x^2+'+str(b)+'x+'+str(c)+'=0:','\nx1','=',x1, '\nx2','=',x2)
+if koren > 0:    
+    print(str(a)+'x^2+'+str(b)+'x+'+str(c)+'=0:','\nx1','=',x1, '\nx2','=',x2)
+elif koren == 0:
+    print(str(a)+'x^2+'+str(b)+'x+'+str(c)+'=0:','\nx1','=',x1,)
+else:
+    print('no roots')
