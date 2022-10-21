2dfd3f827f1958e83c4141ef55d759b01046299d
Stanislav <slipenko40@gmail.com>
2022-10-21 19:33:37 +0300

    changed the name of the variables, removed empty lines, broke a large line

--- Lesson_1/task2.py
+++ Lesson_1/task2.py
@@ -1,29 +1,21 @@
 while True:
-    a = int(input('Enter number a:'))
-    b = int(input('Enter number b:'))
-    z = input('list of possible operations:\n1 - addition\n2 - subtraction\n3 - division\n4 - multiplication\n5 - to the extent\n6 - square root \nSelect the operation number from the list:')
-
+    first_number = int(input('Enter number a:'))
+    second_number = int(input('Enter number b:'))
+    z = input('list of possible operations:\n1 - addition\n2 - subtraction\n'\
+    '3 - division\n4 - multiplication\n5 - to the extent\n6 - square root\n'\
+    'Select the operation number from the list:')
     if z == '1':
-        print(a+b)
-
+        print(first_number+second_number)
     elif z == '2':
-        print(a-b)
-
+        print(first_number-second_number)
     elif z == '3':
-        print(a/b)
-
+        print(first_number/second_number)
     elif z == '4':
-        print(a*b)
-
+        print(first_number*second_number)
     elif z == '5':
-        print(a**b)
-
+        print(first_number**second_number)
     elif z == '6':
-        print(a**(0.5))
-
+        print(first_number**(0.5))
     else:
      print('Error enter operator correctly')
     
- 
-    
-
