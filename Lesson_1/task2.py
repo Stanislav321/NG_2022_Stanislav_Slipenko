c634535f74eff1622c9735eaac0ab5bd1070e287
Stanislav <slipenko40@gmail.com>
2022-10-27 21:04:14 +0300

    split the line

--- Lesson_1/task2.py
+++ Lesson_1/task2.py
@@ -1,9 +1,9 @@
 while True:
     first_number = int(input('Enter number a:'))
     second_number = int(input('Enter number b:'))
-    z = input('list of possible operations:\n1 - addition\n2 - subtraction\n'\
-    '3 - division\n4 - multiplication\n5 - to the extent\n6 - square root\n'\
-    'Select the operation number from the list:')
+    print('list of possible operations:\n1 - addition\n2 - subtraction')
+    print('3 - division\n4 - multiplication\n5 - to the extent\n6 - square root')
+    z = input('Select the operation number from the list:')
     if z == '1':
         print(first_number+second_number)
     elif z == '2':
