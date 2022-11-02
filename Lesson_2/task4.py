c50c8579a4bbeb9133c2c17e079c2913e872ec94
Stanislav <slipenko40@gmail.com>
2022-11-02 20:37:03 +0300

    removed loop and list

--- Lesson_2/task4.py
+++ Lesson_2/task4.py
@@ -1,13 +1,6 @@
 wrote = int(input('The number for the factorial: '))
-list = []
-list2 = []
 list3 = 1
 while wrote>=1:
-    list.append(wrote)
+    list3 *= wrote
     wrote -= 1
-for i in list:
-    list3 = i*list3
-    list2.append(list3)
-print(list2[-1])
-
-    
+print(list3)
