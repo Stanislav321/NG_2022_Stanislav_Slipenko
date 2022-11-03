7f9c5a5531d6b0b969e7e44787e9f2e3bda8c8f6
Stanislav <slipenko40@gmail.com>
2022-11-03 20:00:28 +0300

    used set

--- Lesson_2/task2.py
+++ Lesson_2/task2.py
@@ -1,7 +1,4 @@
 list = input('List Entry: ')
-splist = list.split(',')
-no_copies = []
-for i in splist:
-    if i not in no_copies:
-            no_copies.append(i)
-print(no_copies)
+list = list.split(',')
+list = set(list)
+print(list)
