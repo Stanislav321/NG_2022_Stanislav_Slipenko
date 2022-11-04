ec6c8d5c10fc19e1e265daefc160111528ee9af6
Stanislav <slipenko40@gmail.com>
2022-11-04 11:03:26 +0300

    through cycles

--- We know/dumb_decryptor.py
+++ We know/dumb_decryptor.py
@@ -1,1 +1,12 @@
-https://calculatorium.ru/cryptography/caesar-cipher
+alphabet  =  'ABCDEFGHIJKLMNOPQRSTUVWXYZABCDEFGHIJKLMNOPQRSTUVWXYZ'
+cipher = int(input('Shift on: '))
+inpute = input("text: ").upper()   
+conclusion = ''   
+for i in inpute:
+    once  = alphabet .find(i)
+    once2 = once  + cipher
+    if i in alphabet :
+        conclusion += alphabet [once2]
+    else:
+        conclusion += i
+print (conclusion)
